5b08b6097985e7214c73233bd1cd66f9549520bc
DESKTOP-JVRIQ6C\Richard <rsanchezmieres>
2017-12-09 23:45:52 -0300

    Ajustes
    Add dialogo seleccionar fecha

--- app/src/main/java/richard/com/navigationdrawer_demo/ResultadosFragment.java
+++ app/src/main/java/richard/com/navigationdrawer_demo/ResultadosFragment.java
@@ -1,5 +1,6 @@
 package richard.com.navigationdrawer_demo;
 
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
@@ -9,6 +10,7 @@ import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,6 +26,7 @@ public class ResultadosFragment extends Fragment {
 
     private ProgressDialog progressDialog = null;
     private View vista;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -32,12 +35,37 @@ public class ResultadosFragment extends Fragment {
         vista = inflater.inflate(R.layout.resultados_fragment, container, false);
         vista.setVisibility(View.INVISIBLE);
 
-        progressDialog = UtilsConexion.getProgressDialog(getContext(),"Resultados","Procesando solicitud");
-        AsyncResultados asyncResultados = new AsyncResultados(progressDialog,getContext());
+        progressDialog = UtilsConexion.getProgressDialog(getContext(), "Resultados", "Procesando solicitud");
+        AsyncResultados asyncResultados = new AsyncResultados(progressDialog, getContext());
         asyncResultados.execute("1");
 
         return vista;
     }
+
+    private Dialog dialogo;
+    private Button dConfirmar;
+    private Button dCerrar;
+
+    private void generarDialogo() {
+        dialogo = new Dialog(getContext());
+        dialogo.setContentView(R.layout.select_fecha_layout);
+        dialogo.setTitle("Confirmar Resultados");
+
+        dConfirmar = (Button) dialogo.findViewById(R.id.dialogo_confirmar);
+        dCerrar = (Button) dialogo.findViewById(R.id.dialogo_cerrar);
+        dCerrar.setEnabled(true);
+
+        //listener
+        dCerrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogo.cancel();
+            }
+        });
+
+
+    }
+
 
     public class AsyncResultados extends AsyncTask<String, Void, ResultadosRespuesta> {
 
@@ -77,88 +105,88 @@ public class ResultadosFragment extends Fragment {
             if (respuesta == null) {
                 Toast.makeText(contexto, "Ha ocurrido un error", Toast.LENGTH_LONG).show();
             } else {
-                if(respuesta.getCodigoRespuesta() != 0){
+                if (respuesta.getCodigoRespuesta() != 0) {
                     Toast.makeText(contexto, respuesta.getDescripcionRespuesta(), Toast.LENGTH_LONG).show();
-                }else{
+                } else {
                     vista.setVisibility(View.VISIBLE);
                     byte it = 0;
                     FragmentActivity fragmentActivity = getActivity();
-                    ((TextView)fragmentActivity.findViewById(R.id.req1)).setText("Fecha Nro: " + respuesta.getLstResultados().get(0).getFecha().toString());
-                    for (Resultados resultados : respuesta.getLstResultados()){
+                    ((TextView) fragmentActivity.findViewById(R.id.req1)).setText("Fecha Nro: " + respuesta.getLstResultados().get(0).getFecha().toString());
+                    for (Resultados resultados : respuesta.getLstResultados()) {
                         it++;
-                        switch (it){
+                        switch (it) {
                             case 1:
                                 // NOMBRES DE LOS EQUIPOS
-                                ((TextView)fragmentActivity.findViewById(R.id.req1)).setText(resultados.getEquipoLocal());
-                                ((TextView)fragmentActivity.findViewById(R.id.req2)).setText(resultados.getEquipoVisita());
+                                ((TextView) fragmentActivity.findViewById(R.id.req1)).setText(resultados.getEquipoLocal());
+                                ((TextView) fragmentActivity.findViewById(R.id.req2)).setText(resultados.getEquipoVisita());
 
                                 // MARCADORES
-                                ((TextView)fragmentActivity.findViewById(R.id.gol_local1)).setText((resultados.getGolLocal() != null) ? resultados.getGolLocal().toString() : "S/N");
-                                ((TextView)fragmentActivity.findViewById(R.id.gol_visita1)).setText((resultados.getGolVisita() != null) ? resultados.getGolVisita().toString() : "S/N");
+                                ((TextView) fragmentActivity.findViewById(R.id.gol_local1)).setText((resultados.getGolLocal() != null) ? resultados.getGolLocal().toString() : "0");
+                                ((TextView) fragmentActivity.findViewById(R.id.gol_visita1)).setText((resultados.getGolVisita() != null) ? resultados.getGolVisita().toString() : "0");
 
                                 // GANADOR DEL JUEGO
-                                ((TextView)fragmentActivity.findViewById(R.id.ganador_1)).setText((resultados.getGanador() != null) ? resultados.getGanador().toString() : "EMPATE");
+                                ((TextView) fragmentActivity.findViewById(R.id.ganador_1)).setText((resultados.getGanador() != null) ? resultados.getGanador().toString() : "EMPATE");
 
                                 break;
                             case 2:
                                 // NOMBRES DE LOS EQUIPOS
-                                ((TextView)fragmentActivity.findViewById(R.id.req3)).setText(resultados.getEquipoLocal());
-                                ((TextView)fragmentActivity.findViewById(R.id.req4)).setText(resultados.getEquipoVisita());
+                                ((TextView) fragmentActivity.findViewById(R.id.req3)).setText(resultados.getEquipoLocal());
+                                ((TextView) fragmentActivity.findViewById(R.id.req4)).setText(resultados.getEquipoVisita());
 
                                 // MARCADORES
-                                ((TextView)fragmentActivity.findViewById(R.id.gol_local2)).setText((resultados.getGolLocal() != null) ? resultados.getGolLocal().toString() : "S/N");
-                                ((TextView)fragmentActivity.findViewById(R.id.gol_visita2)).setText((resultados.getGolVisita() != null) ? resultados.getGolVisita().toString() : "S/N");
+                                ((TextView) fragmentActivity.findViewById(R.id.gol_local2)).setText((resultados.getGolLocal() != null) ? resultados.getGolLocal().toString() : "0");
+                                ((TextView) fragmentActivity.findViewById(R.id.gol_visita2)).setText((resultados.getGolVisita() != null) ? resultados.getGolVisita().toString() : "0");
 
                                 // GANADOR DEL JUEGO
-                                ((TextView)fragmentActivity.findViewById(R.id.ganador_2)).setText((resultados.getGanador() != null) ? resultados.getGanador().toString() : "EMPATE");
+                                ((TextView) fragmentActivity.findViewById(R.id.ganador_2)).setText((resultados.getGanador() != null) ? resultados.getGanador().toString() : "EMPATE");
                                 break;
                             case 3:
                                 // NOMBRES DE LOS EQUIPOS
-                                ((TextView)fragmentActivity.findViewById(R.id.req5)).setText(resultados.getEquipoLocal());
-                                ((TextView)fragmentActivity.findViewById(R.id.req6)).setText(resultados.getEquipoVisita());
+                                ((TextView) fragmentActivity.findViewById(R.id.req5)).setText(resultados.getEquipoLocal());
+                                ((TextView) fragmentActivity.findViewById(R.id.req6)).setText(resultados.getEquipoVisita());
 
                                 // MARCADORES
-                                ((TextView)fragmentActivity.findViewById(R.id.gol_local3)).setText((resultados.getGolLocal() != null) ? resultados.getGolLocal().toString() : "S/N");
-                                ((TextView)fragmentActivity.findViewById(R.id.gol_visita3)).setText((resultados.getGolVisita() != null) ? resultados.getGolVisita().toString() : "S/N");
+                                ((TextView) fragmentActivity.findViewById(R.id.gol_local3)).setText((resultados.getGolLocal() != null) ? resultados.getGolLocal().toString() : "0");
+                                ((TextView) fragmentActivity.findViewById(R.id.gol_visita3)).setText((resultados.getGolVisita() != null) ? resultados.getGolVisita().toString() : "0");
 
                                 // GANADOR DEL JUEGO
-                                ((TextView)fragmentActivity.findViewById(R.id.ganador_3)).setText((resultados.getGanador() != null) ? resultados.getGanador().toString() : "EMPATE");
+                                ((TextView) fragmentActivity.findViewById(R.id.ganador_3)).setText((resultados.getGanador() != null) ? resultados.getGanador().toString() : "EMPATE");
                                 break;
                             case 4:
                                 // NOMBRES DE LOS EQUIPOS
-                                ((TextView)fragmentActivity.findViewById(R.id.req7)).setText(resultados.getEquipoLocal());
-                                ((TextView)fragmentActivity.findViewById(R.id.req8)).setText(resultados.getEquipoVisita());
+                                ((TextView) fragmentActivity.findViewById(R.id.req7)).setText(resultados.getEquipoLocal());
+                                ((TextView) fragmentActivity.findViewById(R.id.req8)).setText(resultados.getEquipoVisita());
 
                                 // MARCADORES
-                                ((TextView)fragmentActivity.findViewById(R.id.gol_local4)).setText((resultados.getGolLocal() != null) ? resultados.getGolLocal().toString() : "S/N");
-                                ((TextView)fragmentActivity.findViewById(R.id.gol_visita4)).setText((resultados.getGolVisita() != null) ? resultados.getGolVisita().toString() : "S/N");
+                                ((TextView) fragmentActivity.findViewById(R.id.gol_local4)).setText((resultados.getGolLocal() != null) ? resultados.getGolLocal().toString() : "0");
+                                ((TextView) fragmentActivity.findViewById(R.id.gol_visita4)).setText((resultados.getGolVisita() != null) ? resultados.getGolVisita().toString() : "0");
 
                                 // GANADOR DEL JUEGO
-                                ((TextView)fragmentActivity.findViewById(R.id.ganador_4)).setText((resultados.getGanador() != null) ? resultados.getGanador().toString() : "EMPATE");
+                                ((TextView) fragmentActivity.findViewById(R.id.ganador_4)).setText((resultados.getGanador() != null) ? resultados.getGanador().toString() : "EMPATE");
                                 break;
                             case 5:
                                 // NOMBRES DE LOS EQUIPOS
-                                ((TextView)fragmentActivity.findViewById(R.id.req9)).setText(resultados.getEquipoLocal());
-                                ((TextView)fragmentActivity.findViewById(R.id.req10)).setText(resultados.getEquipoVisita());
+                                ((TextView) fragmentActivity.findViewById(R.id.req9)).setText(resultados.getEquipoLocal());
+                                ((TextView) fragmentActivity.findViewById(R.id.req10)).setText(resultados.getEquipoVisita());
 
                                 // MARCADORES
-                                ((TextView)fragmentActivity.findViewById(R.id.gol_local5)).setText((resultados.getGolLocal() != null) ? resultados.getGolLocal().toString() : "S/N");
-                                ((TextView)fragmentActivity.findViewById(R.id.gol_visita5)).setText((resultados.getGolVisita() != null) ? resultados.getGolVisita().toString() : "S/N");
+                                ((TextView) fragmentActivity.findViewById(R.id.gol_local5)).setText((resultados.getGolLocal() != null) ? resultados.getGolLocal().toString() : "0");
+                                ((TextView) fragmentActivity.findViewById(R.id.gol_visita5)).setText((resultados.getGolVisita() != null) ? resultados.getGolVisita().toString() : "0");
 
                                 // GANADOR DEL JUEGO
-                                ((TextView)fragmentActivity.findViewById(R.id.ganador_5)).setText((resultados.getGanador() != null) ? resultados.getGanador().toString() : "EMPATE");
+                                ((TextView) fragmentActivity.findViewById(R.id.ganador_5)).setText((resultados.getGanador() != null) ? resultados.getGanador().toString() : "EMPATE");
                                 break;
                             case 6:
                                 // NOMBRES DE LOS EQUIPOS
-                                ((TextView)fragmentActivity.findViewById(R.id.req11)).setText(resultados.getEquipoLocal());
-                                ((TextView)fragmentActivity.findViewById(R.id.req12)).setText(resultados.getEquipoVisita());
+                                ((TextView) fragmentActivity.findViewById(R.id.req11)).setText(resultados.getEquipoLocal());
+                                ((TextView) fragmentActivity.findViewById(R.id.req12)).setText(resultados.getEquipoVisita());
 
                                 // MARCADORES
-                                ((TextView)fragmentActivity.findViewById(R.id.gol_local6)).setText((resultados.getGolLocal() != null) ? resultados.getGolLocal().toString() : "S/N");
-                                ((TextView)fragmentActivity.findViewById(R.id.gol_visita6)).setText((resultados.getGolVisita() != null) ? resultados.getGolVisita().toString() : "S/N");
+                                ((TextView) fragmentActivity.findViewById(R.id.gol_local6)).setText((resultados.getGolLocal() != null) ? resultados.getGolLocal().toString() : "0");
+                                ((TextView) fragmentActivity.findViewById(R.id.gol_visita6)).setText((resultados.getGolVisita() != null) ? resultados.getGolVisita().toString() : "0");
 
                                 // GANADOR DEL JUEGO
-                                ((TextView)fragmentActivity.findViewById(R.id.ganador_6)).setText((resultados.getGanador() != null) ? resultados.getGanador().toString() : "EMPATE");
+                                ((TextView) fragmentActivity.findViewById(R.id.ganador_6)).setText((resultados.getGanador() != null) ? resultados.getGanador().toString() : "EMPATE");
                                 break;
                         }
                     }
